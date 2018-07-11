daeb00fc707f8200800d851c2c4a124c2dee2b55
Grant Wallace <gwallace@princeton.edu>
2018-07-11 16:10:15 -0400

    initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rtAttenJS
 A version of the rtAttenPenn client written in JavaScript using Electron and React.
+
+# install
+npm install
+
+# run
+npm start
---
+++ src/main.js
@@ -0,0 +1,9 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const e = React.createElement;
+
+ReactDOM.render(
+  e('h1', null, 'RtAttenJS'),
+  document.getElementById('app')
+)
